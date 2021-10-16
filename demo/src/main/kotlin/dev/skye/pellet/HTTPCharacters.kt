3f7725d17a18408765297685ca7e9048238760a6
Skye Welch <149851+CarrotCodes@users.noreply.github.com>
2021-10-16 23:39:09 +0100

    Playing around with message parsing

---
+++ demo/src/main/kotlin/dev/skye/pellet/HTTPCharacters.kt
@@ -0,0 +1,32 @@
+package dev.skye.pellet
+
+object HTTPCharacters {
+
+    val CHARS = (0..127)
+        .map {
+            it.toChar()
+        }
+        .toSet()
+    val LOALPHA_CHARS = ('a'..'z').toSet()
+    val UPALPHA_CHARS = ('A'..'Z').toSet()
+    val ALPHA_CHARS = LOALPHA_CHARS + UPALPHA_CHARS
+    val DIGIT_CHARS = ('0'..'9').toSet()
+    val CONTROL_CHARS = (0..31).map { it.toChar() }.toSet() + setOf(127.toChar())
+
+    val CARRIAGE_RETURN = 13.toChar()
+    val LINE_FEED = 10.toChar()
+    val SPACE = 32.toChar()
+    val HORIZONTAL_TAB = 9.toChar()
+    val QUOTE = 34.toChar()
+    val COLON = 58.toChar()
+
+    val LWS_CHARS = setOf(SPACE, HORIZONTAL_TAB)
+
+    val BYTES = CHARS.map { it.code.toByte() }.toSet()
+    val LWS_BYTES = LWS_CHARS.map { it.code.toByte() }.toSet()
+    val CARRIAGE_RETURN_BYTE = CARRIAGE_RETURN.code.toByte()
+    val LINE_FEED_BYTE = LINE_FEED.code.toByte()
+    val SPACE_BYTE = SPACE.code.toByte()
+    val HORIZONTAL_TAB_BYTE = HORIZONTAL_TAB.code.toByte()
+    val QUOTE_BYTE = QUOTE.code.toByte()
+}
