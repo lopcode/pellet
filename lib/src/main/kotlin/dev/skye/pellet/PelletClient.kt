a8cf5dd48bb653b578689286fcea75f2dd400403
Carrot <149851+CarrotCodes@users.noreply.github.com>
2021-11-12 22:20:30 +0000

    Close connection if header requests it

--- lib/src/main/kotlin/dev/skye/pellet/PelletClient.kt
+++ lib/src/main/kotlin/dev/skye/pellet/PelletClient.kt
@@ -1,10 +1,21 @@
 package dev.skye.pellet
 
 import dev.skye.pellet.codec.Codec
+import dev.skye.pellet.extension.awaitWrite
+import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 
 class PelletClient(
-    val socket: AsynchronousSocketChannel
+    private val socket: AsynchronousSocketChannel
 ) {
     lateinit var codec: Codec
+
+    suspend fun write(bytes: ByteBuffer): Int {
+        return socket.awaitWrite(bytes)
+    }
+
+    fun close() {
+        logger.info("closed $socket")
+        socket.close()
+    }
 }
