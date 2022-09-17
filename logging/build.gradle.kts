501936eeba9697612beb731663556c7ca55255a4
CarrotCodes <149851+CarrotCodes@users.noreply.github.com>
2022-09-17 16:13:23 +0100

    Fix IntelliJ warnings for version catalog

--- logging/build.gradle.kts
+++ logging/build.gradle.kts
@@ -1,6 +1,15 @@
 
 import java.net.URI
 
+// Without these suppressions version catalog usage here and in other build
+// files is marked red by IntelliJ:
+// https://youtrack.jetbrains.com/issue/KTIJ-19369.
+@Suppress(
+    "DSL_SCOPE_VIOLATION",
+    "MISSING_DEPENDENCY_CLASS",
+    "UNRESOLVED_REFERENCE_WRONG_RECEIVER",
+    "FUNCTION_CALL_EXPECTED"
+)
 plugins {
     `java-library`
     alias(libs.plugins.kotlin.serialization)
